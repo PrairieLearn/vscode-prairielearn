fd141a38da71dde8fd31951b67e0dfdf747af604
Nathan Walters <nwalters512@gmail.com>
2019-05-02 21:01:08 -0500

    Rename extension

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# prairielearn-vscode README
+# vscode-prairielearne README
 
-This is the README for your extension "prairielearn-vscode". After writing up a brief description, we recommend including the following sections.
+This is the README for your extension "vscode-prairielearn". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
--- src/extension.ts
+++ src/extension.ts
@@ -29,7 +29,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
-    'Congratulations, your extension "prairielearn-vscode" is now active!'
+    'Congratulations, your extension "vscode-prairielearn" is now active!'
   );
 
   // The command has been defined in the package.json file
